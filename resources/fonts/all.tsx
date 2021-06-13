5071acd48a91d069d6c629316b608f7f0678e007
cerebellumking <1805219550@qq.com>
2021-06-13 11:49:03 +0800

    szb's update of resources

---
+++ resources/fonts/all.tsx
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="all" tilewidth="120" tileheight="150" tilecount="27" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="60" height="59" source="D:/素材/maps/宝箱.png"/>
+ </tile>
+ <tile id="2">
+  <image width="100" height="57" source="D:/素材/maps/宝箱开.png"/>
+ </tile>
+ <tile id="3">
+  <image width="40" height="40" source="D:/素材/maps/冰地板.png"/>
+ </tile>
+ <tile id="4">
+  <image width="40" height="40" source="D:/素材/maps/冰地板1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="40" height="40" source="D:/素材/maps/冰地板2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="40" height="60" source="D:/素材/maps/冰墙.png"/>
+ </tile>
+ <tile id="7">
+  <image width="40" height="60" source="D:/素材/maps/冰墙1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="120" height="150" source="D:/素材/maps/传送门1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="120" height="150" source="D:/素材/maps/传送门2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="120" height="150" source="D:/素材/maps/传送门3.png"/>
+ </tile>
+ <tile id="11">
+  <image width="120" height="150" source="D:/素材/maps/传送门4.png"/>
+ </tile>
+ <tile id="12">
+  <image width="40" height="40" source="D:/素材/maps/地板.png"/>
+ </tile>
+ <tile id="13">
+  <image width="40" height="40" source="D:/素材/maps/地板2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="40" height="40" source="D:/素材/maps/地板3.png"/>
+ </tile>
+ <tile id="15">
+  <image width="40" height="40" source="D:/素材/maps/地板4.png"/>
+ </tile>
+ <tile id="16">
+  <image width="40" height="80" source="D:/素材/maps/火炬障碍.png"/>
+ </tile>
+ <tile id="17">
+  <image width="40" height="60" source="D:/素材/maps/木箱.png"/>
+ </tile>
+ <tile id="18">
+  <image width="40" height="40" source="D:/素材/maps/骑士地板.png"/>
+ </tile>
+ <tile id="19">
+  <image width="40" height="40" source="D:/素材/maps/骑士地板1.png"/>
+ </tile>
+ <tile id="20">
+  <image width="40" height="40" source="D:/素材/maps/骑士地板2.png"/>
+ </tile>
+ <tile id="21">
+  <image width="40" height="40" source="D:/素材/maps/骑士墙.png"/>
+ </tile>
+ <tile id="22">
+  <image width="40" height="40" source="D:/素材/maps/骑士墙1.png"/>
+ </tile>
+ <tile id="23">
+  <image width="40" height="60" source="D:/素材/maps/墙.png"/>
+ </tile>
+ <tile id="24">
+  <image width="40" height="60" source="D:/素材/maps/墙1.png"/>
+ </tile>
+ <tile id="25">
+  <image width="40" height="80" source="D:/素材/maps/上下门（关）.png"/>
+ </tile>
+ <tile id="26">
+  <image width="40" height="60" source="D:/素材/maps/左右门（关下端）.png"/>
+ </tile>
+ <tile id="27">
+  <image width="40" height="40" source="D:/素材/maps/左右门（开）.png"/>
+ </tile>
+</tileset>
