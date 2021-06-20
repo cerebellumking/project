b61ccf9cc68fcde13e9ce589c21134e3e94f6b2b
cerebellumking <1805219550@qq.com>
2021-06-20 11:57:42 +0800

    commit of resources

--- resources/orange.tsx
+++ resources/orange.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="orange" tilewidth="40" tileheight="40" tilecount="1" columns="1">
- <image source="orange.png" width="64" height="64"/>
+<tileset version="1.5" tiledversion="1.7.0" name="orange" tilewidth="56" tileheight="56" tilecount="1" columns="1">
+ <image source="orange.png" width="56" height="56"/>
  <tile id="0">
   <properties>
    <property name="collidable" type="bool" value="true"/>
